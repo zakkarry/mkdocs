c93a4bf73b842469001a6e63128607e61910907f
zakary <zak@ary.dev>
2024-04-14 14:29:35 -0500

    fix(strict): adds --no-strict flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-sed -i 's/strict: true/#strict: true/g' /docs/mkdocs.yml
-exec /sbin/tini -- mkdocs serve "--dev-addr=0.0.0.0:8000"
+exec /sbin/tini -- mkdocs serve "--no-strict" "--dev-addr=0.0.0.0:8000" 
